a8380db85bf0b5f5ffe305b21515a97667e8aafd
Joshua Benas <45321985+joshbakit@users.noreply.github.com>
2026-06-18 22:50:41 +0800

    chnagin the token expiry

--- apps/web/src/helper/tokenExpiry.tsx
+++ apps/web/src/helper/tokenExpiry.tsx
@@ -3,6 +3,6 @@ import { useAuthStore } from "@/stores/auth.store";
 const IS_PROD = import.meta.env.VITE_NODE_ENV;
 
 export const updateTokenExpiry = () => {
-    const ACCESS_TOKEN_MS = IS_PROD === 'development' ? 30 : 15 * 60 * 1000;
+    const ACCESS_TOKEN_MS = IS_PROD === 'production' ? 30 : 15 * 60 * 1000;
     useAuthStore.getState().setTokenExpiry(Date.now() + ACCESS_TOKEN_MS);
 }
